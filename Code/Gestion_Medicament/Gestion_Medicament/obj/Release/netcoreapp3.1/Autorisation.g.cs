b0e65061353c185f3f05010c9b4d2ae828f64d0b
soudant <Thomas.Soudan@etu.univ-smb.fr>
2022-06-17 13:15:16 +0200

    jsp

--- Code/Gestion_Medicament/Gestion_Medicament/obj/Release/netcoreapp3.1/Autorisation.g.cs
+++ Code/Gestion_Medicament/Gestion_Medicament/obj/Release/netcoreapp3.1/Autorisation.g.cs
@@ -53,7 +53,7 @@ namespace Gestion_Medicament {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Gestion_Medicament;V1.0.0.0;component/autorisation.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Gestion_Medicament;component/autorisation.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Autorisation.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
